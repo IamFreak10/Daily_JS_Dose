a88be281ffe8b8c434e6c3b638864c6e2dcde456
Mahfuj Abdulla <155483252+IamFreak10@users.noreply.github.com>
2026-01-22 03:04:51 +0600

    Array pRacticed

--- daily-practice/Array/array.js
+++ daily-practice/Array/array.js
@@ -13,18 +13,27 @@ const myList2 = fruits.join('->');
 fruits[fruits.length] = 'Kiwi';
 // console.log(fruits);
 
-const arr1 = ["Cecilie", "Lone"];
-const arr2 = ["Emil", "Tobias", "Linus"];
-const arr3 = ["Robin", "Morgan"];
+const arr1 = ['Cecilie', 'Lone'];
+const arr2 = ['Emil', 'Tobias', 'Linus'];
+const arr3 = ['Robin', 'Morgan'];
 const myChildren = arr1.concat(arr2, arr3);
-console.log(myChildren);
+// console.log(myChildren);
 // ** concat()- joins two or more arrays,and returns a new array
-
 
 fruits.copyWithin(2, 0);
 // console.log(fruits);
 fruits.copyWithin(2, 0, 2);
 // console.log(fruits);
-const myArr = [[1,2],[3,4],[5,6],[7,8,[9,10,[11,12]]]];
+const myArr = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+  [7, 8, [9, 10, [11, 12]]],
+];
 const newArr = myArr.flat(Infinity);
-console.log(newArr);
+// console.log(newArr);
+
+const fruits2 = ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango'];
+const citrus = fruits2.slice(3); //?Slice out a part of an array starting from array element 3 ("Apple"):
+const citrus2 = fruits2.slice(1, 2); //?The method then selects elements from the start argument, and up to (but not including) the end argument.["orange"]
+console.log(citrus2);
