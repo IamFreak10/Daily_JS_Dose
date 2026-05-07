8c9a18e4617c7fb651cbfa34182c3f69c8acfd5a
IamFreak10 <mahfujabdulla9@gmail.com>
2026-05-07 09:54:14 +0600

    Added Basic restatring

---
+++ Basic/01.js
@@ -0,0 +1,11 @@
+const accountID = 122222222;
+let accountEmail = 'email@gamil.com';
+var accountPassword = '12345';
+accountNode = 12; //!Also posible
+// accountID = 2; //ashbe na const 
+let alone;
+console.log(alone); //indefiend as nothing was defined
+//! console.log(accountID); const so ashbe na
+// accountPassword="Newly"
+// console.log(acc)
+//? we will use let and const,var have an issues with block and functioanl scope
---
+++ Basic/02.js
@@ -0,0 +1,17 @@
+"use strict" //treat all js code as newer version
+// alert(3+3) vs code e na,browser engine e,we are using node.js
+let name="Hitesh"
+let age=13
+let isLoggedin=true
+// premative data tyeps
+//number 
+//big int
+//String
+// Boolean
+//null=>stanalone value ,empty value
+//undefiend =>Not assigned value
+//symbol=>unique
+
+//object
+// console.log(typeof null) object
+// console.log(typeof undefined) undefined
