f04886f2e3338cdd91e10ea83eaefe2a37de70d8
Mahfuj Abdullah <mahfujabdulla9@gmail.com>
2026-06-26 20:46:56 +0000

    Added datatypes summary

---
+++ Basic/comparison.js
@@ -0,0 +1,14 @@
+// console.log("3">2);
+// console.log("02">1);
+//convert automatically
+
+console.log(null>0);
+console.log(null==0); 
+console.log(null>=0);
+//comparison convert null into a 0,null is treated as 0 tahts why null>=0 is true null>0 false but comparison works differently
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+console.log("2"===2);
---
+++ Basic/datatypes_summary.js
@@ -0,0 +1,25 @@
+//call by values and call by reference,primitive and non primitive
+//Primitive:
+//7 catagory:string,Number,Boolean,null,undefined,symbol(Unique value,component can be unique in react.js) BigInt
+
+//Reference Type(Non Primitive)
+//Array,Objects,Functions
+
+
+//Is Javascript Dynamic or static type?? dynamic datatype,singlethreaded,interprated,multipradiagram
+
+const heroes=["Man","Woman","Cat"]
+
+let myObj={
+    name:"Biral",
+    age:3
+}
+
+const myFunction=function(){
+    console.log("Print Hello");
+}
+
+console.log(typeof myFunction);
+console.log(typeof null)
+console.log(typeof NaN)
+console.log(typeof myObj)
